a86c4c0685bcd842d2289d2776ff63cb47c0abdb
Sang Kha <80106348+khasang12-khmt@users.noreply.github.com>
2023-09-17 12:18:42 +0700

    add top layout

---
+++ app/src/main/java/com/example/ecommerce_app/fragments/categories/BaseCategoryFragment.kt
@@ -0,0 +1,7 @@
+package com.example.ecommerce_app.fragments.categories
+
+import androidx.fragment.app.Fragment
+
+open class BaseCategoryFragment: Fragment() {
+
+}
--- app/src/main/java/com/example/ecommerce_app/fragments/shop/HomeFragment.kt
+++ app/src/main/java/com/example/ecommerce_app/fragments/shop/HomeFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ecommerce_app.R
+import com.example.ecommerce_app.adapters.HomeViewpagerAdapter
 import com.example.ecommerce_app.databinding.FragmentHomeBinding
+import com.example.ecommerce_app.fragments.categories.*
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.common.collect.Table
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
@@ -19,4 +22,33 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initTabLayout()
+    }
+
+    private fun initTabLayout() {
+        val categoryFragments = arrayListOf<Fragment>(
+            MainCategoryFragment(),
+            ChairFragment(),
+            CupboardFragment(),
+            TableFragment(),
+            AccessoryFragment(),
+            FurnitureFragment()
+        )
+
+        val viewpagerAdapter = HomeViewpagerAdapter(categoryFragments, childFragmentManager, lifecycle)
+        binding.vpHome.adapter = viewpagerAdapter
+        TabLayoutMediator(binding.homeTabLayout,binding.vpHome){ tab, position ->
+            when(position){
+                0->tab.text="Main"
+                1->tab.text="Chair"
+                2->tab.text="Cupboard"
+                3->tab.text="Table"
+                4->tab.text="Accessory"
+                5->tab.text="Furniture"
+            }
+        }.attach()
+    }
 }
