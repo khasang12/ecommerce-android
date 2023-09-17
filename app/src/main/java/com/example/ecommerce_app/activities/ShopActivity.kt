3c30e62478b778913e08a728ea3e065217afc2f2
Sang Kha <80106348+khasang12-khmt@users.noreply.github.com>
2023-09-17 14:25:19 +0700

    MainCategoryFragment.kt: add recyclerviews

--- app/src/main/java/com/example/ecommerce_app/activities/ShopActivity.kt
+++ app/src/main/java/com/example/ecommerce_app/activities/ShopActivity.kt
@@ -2,10 +2,14 @@ package com.example.ecommerce_app.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.ecommerce_app.R
 import com.example.ecommerce_app.databinding.ActivityShopBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ShopActivity : AppCompatActivity() {
     val binding by lazy {
@@ -16,7 +20,8 @@ class ShopActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop)
 
-        val navController = findNavController(R.id.shoppingHostFragment)
-        binding.bottomNavigation.setupWithNavController(navController)
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = Navigation.findNavController(this, R.id.shoppingHostFragment)
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
--- app/src/main/java/com/example/ecommerce_app/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/com/example/ecommerce_app/fragments/categories/MainCategoryFragment.kt
@@ -1,7 +1,20 @@
 package com.example.ecommerce_app.fragments.categories
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.ecommerce_app.databinding.FragmentMainCategoryBinding
 
 class MainCategoryFragment: Fragment() {
-
+    private lateinit var binding: FragmentMainCategoryBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainCategoryBinding.inflate(inflater)
+        return binding.root
+    }
 }
