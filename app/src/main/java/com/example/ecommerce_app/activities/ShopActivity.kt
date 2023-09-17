1eed7fda162bdd2bb726907991cab8215e45800e
Sang Kha <80106348+khasang12-khmt@users.noreply.github.com>
2023-09-17 09:59:40 +0700

    add bottom navbar

--- app/src/main/java/com/example/ecommerce_app/activities/ShopActivity.kt
+++ app/src/main/java/com/example/ecommerce_app/activities/ShopActivity.kt
@@ -2,11 +2,21 @@ package com.example.ecommerce_app.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.ecommerce_app.R
+import com.example.ecommerce_app.databinding.ActivityShopBinding
 
 class ShopActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivityShopBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop)
+
+        val navController = findNavController(R.id.shoppingHostFragment)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
